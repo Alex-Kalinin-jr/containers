5da1796797943a2220981e56057df2ab5d129a68
Curse Bowman <cursebow@in-f1.nsk.21-school.ru>
2022-10-05 18:41:10 +0700

    branch switched

--- src/stack.h
+++ src/stack.h
@@ -24,24 +24,40 @@ template <typename T> class Node {
             return *this;
         }
 
-        // Node & operator=(Node && other) {
-        //     elem = other.elem;
-        //     back = other.back;
-        //     fwd = other.fwd;
-        // std::cout<<"the move is invoked"<<std::endl;
-        //     return *this;
-        // }
+        Node & operator=(Node && other) {
+            elem = other.elem;
+            back = other.back;
+            fwd = other.fwd;
+        std::cout<<"the move is invoked"<<std::endl;
+            return *this;
+        }
 };
 
-template<typename T> class Stack {
+template<typename T> class Stark {
     using value_type = T;
     using reference = T &;
     using const_reference = const T &;
     using size_type = size_t;
 
     public:
-        Stack() : head(nullptr) {};
-        Stack(std::initializer_list<value_type> buff) {
+        Stark() {
+            std::cout<<"is invoked 1"<<std::endl;
+            sizeOf = 0;
+            head = Node<value_type>();
+        };
+
+        Stark(const Stark &s) {
+            std::cout<<"is invoked 2"<<std::endl;
+            head = s.head;
+            sizeOf = s.sizeOf;
+        }
+
+        Stark(Stark &&s) {
+            head = s.head;
+            sizeOf = s.sizeOf;
+        }
+
+        Stark(std::initializer_list<value_type> buff) {
             for (const value_type * start = buff.begin(); start != buff.end(); ++start) {
                 Node<value_type> node(start);
                 node.back = (start == buff.begin()) ? nullptr : &head;
@@ -50,26 +66,20 @@ template<typename T> class Stack {
             }
         }
 
-        Stack(const Stack &s) {
-            head = s.head;
-            sizeOf = s.sizeOf;
-        }
+        ~Stark<T>() {};
 
-        Stack(Stack &&s) {
-            head = s.head;
-            sizeOf = s.sizeOf;
-        }
-
-        ~Stack<T>() {};
-
-        Stack & operator=(Stack &&s) {
+        Stark & operator=(Stark &&s) {
             head = s.head;
             sizeOf = s.sizeOf;
             return *this;
         }
 
+        const_reference top() {return *(head.elem);}
+
         bool empty() { return (&head == nullptr); }
+
         size_type size() { return sizeOf; }
+
         void push(const_reference value) {
             Node<value_type> chunk(value);
             chunk.back = (&head == nullptr) ? nullptr : &head;
@@ -88,7 +98,7 @@ template<typename T> class Stack {
             }
         }
 
-        void swap(Stack & other) {
+        void swap(Stark & other) {
             size_type buff_size = sizeOf;
             Node<value_type> buff_head = head;
             sizeOf = other.sizeOf;
@@ -97,10 +107,9 @@ template<typename T> class Stack {
             other.head = buff_head;
         }
 
-        const_reference top() {return *(head.elem);}
 
     private:
         Node<value_type> head;
-        size_type sizeOf = 0;
+        size_type sizeOf;
 };
 
--- src/test.cc
+++ src/test.cc
@@ -1,23 +1,19 @@
 // #include <gtest>
-#include "stack.h"
+#include "Stack.h"
 
 int main() {
-    Stack<int> c({5, 4, 3, 2});
-    Stack<int> d({5, 4, 3, 2});
-    bool g = c.empty();
-    std::cout<<" "<<c.size()<<std::endl;
-    c.push(4);
-    c.push(4);
-    c.push(4);
-    std::cout<<" "<<c.size()<<std::endl;
-    c.pop();
-    std::cout<<" "<<c.size()<<std::endl;
-    c.pop();
-    std::cout<<" "<<c.size()<<std::endl;
-    c.swap(d);
-    std::cout<<" "<<c.size()<<std::endl;
-    std::cout<<" "<<c.top()<<std::endl;
-    c = Stack<int>({3, 2, 1});
-    std::cout<<" "<<c.top()<<std::endl;
+    Stark<int> c({5, 4, 3, 2});
+    Stark<int> d;
+    d.push(5);
+    d.push(10);
+    std::cout<<" "<<d.size()<<std::endl;
+    std::cout<<" "<<d.top()<<std::endl;
+    d.pop();
+    d.push(11);
+    std::cout<<" "<<d.size()<<std::endl;
+    std::cout<<" "<<d.top()<<std::endl;
+        d.pop();
+    std::cout<<" "<<d.size()<<std::endl;
+    std::cout<<" "<<d.top()<<std::endl;
     return 0;
 }
