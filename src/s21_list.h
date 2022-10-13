ccfe811426b779764938ed08ab37941b74fcb82f
Alexey <alexey@debian.pro>
2022-10-13 22:44:58 +0700

    init_list perfmd

--- src/s21_list.h
+++ src/s21_list.h
@@ -10,21 +10,36 @@ template<typename T> class s21_List {
     friend node;
 
 public:
-    s21_List() : begin(0), end(0), capacity(0) {};
-    s21_List(size_type n, ) : capacity(n) {
-        node_iterator prev = new node();
+    s21_List() : begin(0), end(0), sizeOf(0) {};
+    s21_List(size_type n, const_reference el) : sizeOf(n) {
+        node_iterator prev = new node(el);
         begin = prev;
         while (n-- > 0) {
-            node_iterator buff = new node();
+            node_iterator buff = new node(el);
             buff->back = prev;
             prev->fwd = buff;
             prev = buff;
         }
         end = prev;
     }
+    s21_List(std::initializer_list<value_type> const &items) : s21_List() {
+        auto el = items.begin();
+        node_iterator prev = new node(*el);
+        begin = prev;
+        ++sizeOf;
+        while ((++el) != items.end()) {
+            node_iterator buff = new node(*el);
+            buff->back = prev;
+            prev->fwd = buff;
+            prev = buff;
+            ++sizeOf;
+        }
+        end = prev;
+    }
+
     ~s21_List() {
         node_iterator prev;
-        while (capacity-- > 0) {
+        while (sizeOf-- > 0) {
             prev = end->back;
             if (end) delete end;
             end = prev;
@@ -32,13 +47,12 @@ public:
         delete end;
     }
 
-    size_type size() {return capacity;}
-    bool empty() {return ((capacity == 0) && (begin == 0));}
+    size_type size() {return sizeOf;}
+    bool empty() {return ((sizeOf == 0) && (begin == 0));}
 
 private:
     node_iterator begin;
     node_iterator end;
-    size_type capacity;
-    size_type size;
+    size_type sizeOf;
 
 };
--- src/test_list.cc
+++ src/test_list.cc
@@ -5,11 +5,34 @@
 TEST(list, init_1) {
     s21_List<int> a;
     ASSERT_TRUE(a.empty());
+
+    s21_List<int> b(5, 4);
+    ASSERT_EQ(b.size(), 5);
 }
 
 TEST(list, init_2) {
-    s21_List<int> a(5);
-    ASSERT_EQ(a.size(), 5);
+    s21_List<int> b({5, 4});
+    ASSERT_EQ(b.size(), 2);
+
+    s21_List<int> d{5, 4};
+    ASSERT_EQ(d.size(), 2);
+}
+
+TEST(list, init_3) {
+    s21_List<std::string> c({"afdfdf", "dfdh", "djfhd", "dkfjd", "dhfd"});
+    ASSERT_EQ(c.size(), 5);
+
+    s21_List<double> e{5, 4, 3, 2, 1};
+    ASSERT_EQ(e.size(), 5);
+
+    s21_List<double> f{{5, 4, 3, 2, 1}};
+    ASSERT_EQ(f.size(), 5);
+
+    s21_List<double> g({5, 4, 3, 2, 1});
+    ASSERT_EQ(f.size(), 5);
+
+    s21_List<double> h({{5, 4, 3, 2, 1}});
+    ASSERT_EQ(h.size(), 5);
 }
 
 int main(int argc, char *argv[]) {
