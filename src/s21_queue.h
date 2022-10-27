3cfbc94f64c28d3ed99e181a37e0e3698ccec16d
Alexey <alexey@debian.pro>
2022-10-27 21:55:50 +0700

    t

---
+++ src/s21_queue.h
@@ -0,0 +1,131 @@
+#ifndef _CONTAINERS_SRC_S21_QUEUE_
+#define _CONTAINERS_SRC_S21_QUEUE_
+#include <iostream>
+
+#include "s21_node.h"
+#include "s21_iterator.h"
+
+namespace s21 {
+
+/* remember that queue is an method to organize data ordering,
+therefore it stores and handles the order of elements, not the elements
+themselves/ If you put a VAR into queue and then change this VAR somwhere else,
+it is changed in queue too. For exmpl, when you try int d = 0; while (++d <
+1000) queue.push(d) the last 1000 elements are all the "d" and are all equal to
+1000*/
+template <typename T>
+class Queue {
+    using value_type = T;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = size_t;
+    using node = s21_Node<value_type>;
+    using node_ptr = s21_Node<value_type> *;
+
+   public:
+    Queue() : head(nullptr), tail(nullptr), sizeOf(0){};
+    Queue(const Queue &s) : Queue() { *this = s; };
+    Queue(Queue &&s) : head(s.head), tail(s.tail), sizeOf(s.sizeOf){};
+    Queue(std::initializer_list<value_type> buff) : Queue() {
+        for (const value_type *start = buff.begin(); start < buff.end();
+             ++start) {
+            push(*start);
+        }
+    }
+    ~Queue() {
+        while (sizeOf > 0) {
+            pop();
+        }
+    };
+
+    Queue &operator=(Queue &&s) {
+        head = s.head;
+        tail = s.tail;
+        sizeOf = s.sizeOf;
+        return *this;
+    }
+
+    Queue &operator=(const Queue &s) {
+        while (!empty()) {
+            pop();
+        }
+
+        if (s.head != nullptr) {
+            s21_Node<value_type> *chunk = new node(*(s.head));
+            head = chunk;
+            tail = chunk;
+            sizeOf++;
+            // HERE TO BE CAREFUL
+            node_ptr iterator(head);
+            while (sizeOf < s.sizeOf) {
+                chunk = new s21_Node<value_type>(*(chunk->back));
+                sizeOf++;
+                chunk->fwd = iterator;
+                iterator->back = chunk;
+                iterator = iterator->back;
+            }
+        }
+        return *this;
+    }
+
+    const_reference front() {
+        if (empty())
+            throw std::out_of_range("Queue::front() - the Queue is empty");
+        return head->get_elem();
+    }
+
+    const_reference back() {
+        if (empty())
+            throw std::out_of_range("Queue::back() - the Queue is empty");
+        return tail->get_elem();
+    }
+
+    bool empty() { return (head) ? false : true; }
+
+    size_type size() { return sizeOf; }
+
+    void push(const_reference value) {
+        node_ptr chunk = new s21_Node<value_type>(value);
+        if (head == nullptr) {
+            head = chunk;
+            tail = chunk;
+        } else {
+            chunk->fwd = tail;
+            tail->back = chunk;
+            tail = chunk;
+        }
+        ++sizeOf;
+    }
+
+    void pop() {
+        if (head != nullptr) {
+            node_ptr buffNode = head;
+            if (tail == head) tail = nullptr;
+            head = head->back;
+            delete buffNode;
+            --sizeOf;
+        }
+    }
+
+    void swap(Queue &other) {
+        if (this != &other) {
+            size_type buff_size = sizeOf;
+            node_ptr buff_head = head;
+            node_ptr buff_tail = tail;
+            sizeOf = other.sizeOf;
+            head = other.head;
+            tail = other.tail;
+            other.sizeOf = buff_size;
+            other.head = buff_head;
+            other.tail = buff_tail;
+        }
+    }
+
+   private:
+    node_ptr head;
+    node_ptr tail;
+    size_type sizeOf;
+};
+
+}  // namespace s21
+#endif  // _CONTAINERS_SRC_S21_QUEUE_
---
+++ src/s21_set.h
@@ -0,0 +1,277 @@
+#ifndef SRC_S21_SET_H_
+#define SRC_S21_SET_H_
+
+#include <climits>
+#include <cstddef>
+#include <initializer_list>
+#include <utility>
+
+#include "s21_node.h"
+#include "s21_iterator.h"
+
+namespace s21 {
+
+template <class Key>
+class set {
+   public:
+    using key_type = Key;
+    using value_type = Key;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using iterator = SetIterator<Key>;
+    using const_iterator = SetConstIterator<Key>;
+    using size_type = size_t;
+
+    set() {}
+
+    set(std::initializer_list<value_type> const& items) {
+        for (auto it = items.begin(); it != items.end(); ++it) {
+            insert(*it);
+        }
+    }
+
+    set(const set& s) : size_(s.size_) { CopyTree(s.root_); }
+
+    set(set&& s) : size_(s.size_), root_(s.root_) {
+        s.size_ = 0;
+        s.root_ = nullptr;
+    }
+
+    ~set() { DeleteTree(root_); }
+
+    void operator=(set&& s) {
+        if (this != &s) {
+            size_ = s.size_;
+            CopyTree(s.root_);
+        }
+    }
+
+    iterator begin() {
+        Node<Key>* node = root_;
+        while (node != nullptr && node->left != nullptr) {
+            node = node->left;
+        }
+        return iterator(node);
+    }
+
+    iterator end() { return iterator(end_); }
+    const_iterator cbegin() { return begin(); }
+    const_iterator cend() { return end(); }
+
+    bool empty() { return size_ == 0; }
+    size_type size() { return size_; }
+    size_type max_size() { return max_size_; }
+
+    void clear() {
+        DeleteTree(root_);
+        size_ = 0;
+        root_ = nullptr;
+    }
+
+    virtual std::pair<iterator, bool> insert(const value_type& value) {
+        if (size_ >= max_size_) {
+            return std::make_pair(iterator(nullptr), false);
+        }
+        Node<Key>* node = new Node<Key>(value);
+        std::pair<iterator, bool> result = std::make_pair(iterator(node), true);
+        ++size_;
+
+        if (root_ == nullptr) {
+            end_ = new Node<Key>;
+            node->right = end_;
+            node->right->parent = node;
+            root_ = node;
+        } else {
+            Node<Key>* tmp = root_;
+            while (tmp != end_ || tmp != nullptr) {
+                if (value < tmp->value) {
+                    if (tmp->left == nullptr) {
+                        tmp->left = node;
+                        node->parent = tmp;
+                        break;
+                    } else {
+                        tmp = tmp->left;
+                    }
+                } else if (value > tmp->value) {
+                    if (tmp->right == end_) {
+                        tmp->right = node;
+                        node->parent = tmp;
+                        end_->parent = node;
+                        node->right = end_;
+                        break;
+                    } else if (tmp->right == nullptr) {
+                        tmp->right = node;
+                        node->parent = tmp;
+                        break;
+                    } else {
+                        tmp = tmp->right;
+                    }
+                } else {
+                    result = std::make_pair(iterator(nullptr), false);
+                    --size_;
+                    delete node;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    void erase(iterator pos) {
+        if (pos == end()) {
+            throw std::out_of_range("invalid pointer");
+        }
+        Node<Key>* node = pos.get_node();
+        if ((node != nullptr && node != end_) ||
+            (node->left == nullptr && node->right == nullptr)) {
+            if (node->left == nullptr && node->right == nullptr) {
+                if (node->parent != nullptr) {
+                    if (node->parent->left == node) {
+                        node->parent->left = nullptr;
+                    }
+                    if (node->parent->right == node) {
+                        node->parent->right = nullptr;
+                    }
+                } else {
+                    root_ = nullptr;
+                    delete end_;
+                    end_ = nullptr;
+                }
+            } else if (node->left == nullptr) {
+                if (node->parent != nullptr) {
+                    if (node->parent->left == node) {
+                        node->parent->left = node->right;
+                    }
+                    if (node->parent->right == node) {
+                        node->parent->right = node->right;
+                    }
+                    node->right->parent = node->parent;
+                } else {
+                    node->right->parent = nullptr;
+                    root_ = node->right;
+                }
+            } else if (node->right == nullptr || node->right == end_) {
+                if (node->parent != nullptr) {
+                    if (node->parent->left == node) {
+                        node->parent->left = node->left;
+                    }
+                    if (node->parent->right == node) {
+                        node->parent->right = node->left;
+                    }
+                    node->left->parent = node->parent;
+                    node->left->right = end_;
+                    node->left->right->parent = node->left;
+                    node->parent = nullptr;
+                } else {
+                    node->left->parent = nullptr;
+                    root_ = node->left;
+                }
+            } else {
+                Node<Key>* tmp = node->right;
+                while (tmp->left != nullptr) {
+                    tmp = tmp->left;
+                }
+                tmp->left = node->left;
+                tmp->left->parent = tmp;
+                if (node->parent != nullptr) {
+                    if (node->parent->left == node) {
+                        node->parent->left = node->right;
+                        node->right->parent = node->parent;
+                    }
+                    if (node->parent->right == node) {
+                        node->parent->right = node->right;
+                        node->right->parent = node->parent;
+                    }
+                } else {
+                    node->right->parent = nullptr;
+                    root_ = node->right;
+                }
+            }
+            --size_;
+            delete node;
+        }
+    }
+
+    void swap(set& other) {
+        if (this != &other) {
+            Node<Key>* buff = other.root_;
+            size_type buff_size = other.size_;
+            other.size_ = size_;
+            other.root_ = root_;
+            root_ = buff;
+            size_ = buff_size;
+        }
+    }
+
+    void merge(set& other) {
+        for (iterator it = other.begin(); it != other.end();) {
+            if (!contains(it.value())) {
+                insert(it.value());
+                other.erase(it);
+                it = other.begin();
+            } else {
+                ++it;
+            }
+        }
+    }
+
+    bool contains(const Key& key) {
+        Node<Key>* node = root_;
+        while (node != nullptr && node->value != key) {
+            if (key < node->value) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+        }
+        return node != nullptr;
+    }
+
+    iterator find(const Key& key) {
+        Node<Key>* node = root_;
+        while (node != nullptr && node->value != key) {
+            if (key < node->value) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+        }
+        return iterator(node);
+    }
+
+   protected:
+    Node<Key>* end_ = nullptr;
+
+    size_type size_ = 0;
+    size_type max_size_ = LLONG_MAX / sizeof(value_type);
+    Node<Key>* root_ = nullptr;
+
+    void CopyTree(Node<Key>* node) {
+        if (node == nullptr) return;
+
+        insert(node->value);
+
+        if (node->left != nullptr) {
+            CopyTree(node->left);
+        }
+        if (node->right != nullptr) {
+            CopyTree(node->right);
+        }
+    }
+
+    void DeleteTree(Node<Key>* node) {
+        if (node == nullptr) return;
+
+        if (node->left != nullptr) {
+            DeleteTree(node->left);
+        }
+        if (node->right != nullptr) {
+            DeleteTree(node->right);
+        }
+        delete node;
+    }
+};
+
+}  // namespace s21
+
+#endif  // SRC_S21_SET_H_
