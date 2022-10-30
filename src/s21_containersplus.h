c1850aeabf03934fb66f0d4a0ea40d3b6ff0d565
Алексей Чернов <gcordell@student.21-school.ru>
2022-10-30 15:37:46 +0700

    Add define

--- src/s21_containersplus.h
+++ src/s21_containersplus.h
@@ -1,2 +1,7 @@
-#include "array.h"
-#include "multiset.h"
+#ifndef S21_CONTAINERSPLUS_H_
+#define S21_CONTAINERSPLUS_H_
+
+#include "s21_array.h"
+#include "s21_multiset.h"
+
+#endif // S21_CONTAINERSPLUS_H_
